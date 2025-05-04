652398f1c34645e6600d62c60db47cbd51c7168a
Arehucaz <guillermogonzalezweller@MacBook-Pro-de-Arehucaz.local>
2025-05-04 12:42:44 +0100

    cambios por error de Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,9 +34,7 @@ export default function RootLayout({
           </div>
         </div>
         <div className="md:hidden">
-          <SessionProvider>
-            {children}
-          </SessionProvider>
+          {children}
         </div>
       </body>
     </html>
