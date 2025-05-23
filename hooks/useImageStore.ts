6b4e9a5a3897c3a5b05f21ac4c4ec1f50d37f756
Arehucaz <guillermogonzalezweller@MacBook-Pro-de-Arehucaz.local>
2025-05-23 09:25:16 +0100

    1.9.2 phash en creación

--- hooks/useImageStore.ts
+++ hooks/useImageStore.ts
@@ -6,8 +6,8 @@ interface ImageStore {
   setOriginalImage: (img: string | null) => void;
   setCroppedImage: (img: string | null) => void;
   clear: () => void;
-  phash?: string | null;
-  setPhash?: (phash: string | null) => void;
+  phash: string | null;
+  setPhash: (phash: string | null) => void;
 }
 
 export const useImageStore = create<ImageStore>((set: (fn: (state: ImageStore) => Partial<ImageStore>) => void) => ({
@@ -17,5 +17,5 @@ export const useImageStore = create<ImageStore>((set: (fn: (state: ImageStore) =
   setCroppedImage: (img: string | null) => set(() => ({ croppedImage: img })),
   clear: () => set(() => ({ originalImage: null, croppedImage: null })),
   phash: null,
-  setPhash: (phash) => set({ phash }),
+  setPhash: (phash) => set(() => ({ phash })),
 })); 
